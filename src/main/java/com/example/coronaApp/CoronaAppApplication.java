8f410309772971fb79811be322c63dc4553a0a51
wiebe-b-1994 <wiebehb12@gmail.com>
2020-04-22 11:13:10 +0200

    Project startup

---
+++ src/main/java/com/example/coronaApp/CoronaAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.coronaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoronaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/coronaApp/CoronaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coronaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoronaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
